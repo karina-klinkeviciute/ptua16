8aba31707ed6eca9f78e3d99d72fa88a2a35e2b5
karina <karina.klinkeviciute@gmail.com>
2024-10-28 21:58:57 +0200

    first lesson

---
+++ lesson1-operators.py
@@ -0,0 +1,118 @@
+print("Hello, group")
+a = 5.5
+b = 25.3
+a2 = 6.2
+a2_1 = 7
+my_name = "Karina"
+print(my_name)
+my_name = "Karina Klinkevičiūtė"
+print(my_name)
+
+print(type(a))
+print(type(a2))
+print(type(my_name))
+
+print(a+b)
+
+print(a+b)
+
+print("hello " + my_name)
+
+c = a + b
+print(c)
+
+c = a - b
+print(c)
+
+c = a * b
+print(c)
+
+c = a / b
+print(c)
+
+c = a ** b
+print(c)
+
+c = a // b
+print(c)
+
+c = a % b
+
+print(c)
+
+letter = "a"
+
+name = "Code academy!!!"
+
+print(name[0])
+
+print(name[1])
+
+print(name[4])
+
+print(name[-1])
+print(name[-2])
+print(name[5:12])
+
+print(name[5:])
+
+print(name[:5])
+
+print(name[1:12:2])
+
+print(name[5::2])
+
+print(name[::-1])
+
+print(name.split())
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(name.replace("C", "k"))
+
+print(name.replace("Code", "Music"))
+
+word1 = "Code"
+
+word2 = "Academy"
+
+phrase = word1 + " " + word2
+print(phrase)
+
+# This way is the best
+phrase = f"Welcome to {word1} {word2}!"
+print(phrase)
+
+print(word1, word2)
+print(a, b, c)
+
+phrase = "Welcome to {} {}".format(word1, word2)
+print(phrase)
+phrase = "Welcome to {1} {0}".format(word1, word2)
+print(phrase)
+
+phrase = "Welcome to %s %s" % (word1, word2)
+
+print(phrase)
+
+var1 = "45"
+var2 = "25"
+
+var1_number = int(var1)
+var2_number = int(var2)
+
+var3 = var1 + var2
+var3_number = var1_number + var2_number
+print(var3)
+print(var3_number)  # this is a comment
+
+var1_float = float(var1)
+print(var1_float)
+
+# we can't turn str with letters into int
+# int("some text")
+
+print(str(5) + str(7))
+
