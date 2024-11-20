d335b6f84056254b6c5dcafa6c837e8cf89bf00f
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2024-11-20 18:38:00 +0200

    typo

--- conditionals.py
+++ conditionals.py
@@ -92,6 +92,6 @@ if not s2 == "c":
 def check_positive(number):
     """check if number is positive"""
     if number >= 0:
-        # if it's more than zeoro or zero, it's cosidered positive
+        # if it's more than zeoro or zero, it's considered positive
         return True
     return False
